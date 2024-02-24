3bf53587ab5f5d0b27ef68d81a225298fb2e33ce
ZhihangLin <93102378+ZhihangLin@users.noreply.github.com>
2024-02-23 21:49:55 -0500

    Update firebase.js

--- Too_Good_To_Good/src/firebase.js
+++ Too_Good_To_Good/src/firebase.js
@@ -1,10 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB6NHUSapo-Dp0e-cmh1GBDSGk6sxsoRxE",
-    authDomain: "challenge-697e4.firebaseapp.com",
-    databaseURL: "https://challenge-697e4.firebaseio.com",
-    projectId: "challenge-697e4",
-    storageBucket: "challenge-697e4.appspot.com",
-    messagingSenderId: "343626220756",
-    appId: "1:343626220756:web:a3d18ee39934732ae22741",
-    measurementId: "G-WF4P52Z2GL"
-  };
+  apiKey: "AIzaSyBzLK6CZjo7zs78rdmv3FRVCt73wTOTXfI",
+  authDomain: "too-good-to-good.firebaseapp.com",
+  projectId: "too-good-to-good",
+  storageBucket: "too-good-to-good.appspot.com",
+  messagingSenderId: "432044374374",
+  appId: "1:432044374374:web:faf0980e2ee0f3667a30a7",
+  measurementId: "G-8L3BV865GC"
+};
