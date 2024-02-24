cc44e701f324bd5ad7abcb9ddb1535edc24aa67a
ZZ201 <44413062+ZZ201@users.noreply.github.com>
2024-02-23 20:24:40 -0500

    Open React Web
    Start of Project!

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <hi> SMD boys! </hi>
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6NHUSapo-Dp0e-cmh1GBDSGk6sxsoRxE",
+    authDomain: "challenge-697e4.firebaseapp.com",
+    databaseURL: "https://challenge-697e4.firebaseio.com",
+    projectId: "challenge-697e4",
+    storageBucket: "challenge-697e4.appspot.com",
+    messagingSenderId: "343626220756",
+    appId: "1:343626220756:web:a3d18ee39934732ae22741",
+    measurementId: "G-WF4P52Z2GL"
+  };
