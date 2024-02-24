8537bc5444d03bcaa6f0f096e55df0a49dd8100c
ZZ201 <44413062+ZZ201@users.noreply.github.com>
2024-02-23 22:32:22 -0500

    tree2
    1

--- Too_Good_To_Good/src/firebase.js
+++ Too_Good_To_Good/src/firebase.js
@@ -1,3 +1,4 @@
+/*
 const firebaseConfig = {
     apiKey: "AIzaSyB6NHUSapo-Dp0e-cmh1GBDSGk6sxsoRxE",
     authDomain: "challenge-697e4.firebaseapp.com",
@@ -7,4 +8,15 @@ const firebaseConfig = {
     messagingSenderId: "343626220756",
     appId: "1:343626220756:web:a3d18ee39934732ae22741",
     measurementId: "G-WF4P52Z2GL"
-  };
+  }; */
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzLK6CZjo7zs78rdmv3FRVCt73wTOTXfI",
+  authDomain: "too-good-to-good.firebaseapp.com",
+  projectId: "too-good-to-good",
+  storageBucket: "too-good-to-good.appspot.com",
+  messagingSenderId: "432044374374",
+  appId: "1:432044374374:web:faf0980e2ee0f3667a30a7",
+  measurementId: "G-8L3BV865GC"
+};
