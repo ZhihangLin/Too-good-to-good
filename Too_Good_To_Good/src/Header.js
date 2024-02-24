43413a1a20a273f93ceb206c2d4d71336ff1152c
ZZ201 <44413062+ZZ201@users.noreply.github.com>
2024-02-23 21:44:17 -0500

    TreeNewBee
    !

--- Too_Good_To_Good/src/Header.js
+++ Too_Good_To_Good/src/Header.js
@@ -3,9 +3,10 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-      <img
+       <img
       className='header__logo'
-      src='https://www.tripfiction.com/wp-content/uploads/2016/08/1920x1080-brands-amazon-logo.jpg' />
+      src={require('./Toogoodtogo.png')}
+      alt='Too Good To Go Logo'/>
       
       <div className='header__search'>
         <input className='header__searchInPut' type='text' />
