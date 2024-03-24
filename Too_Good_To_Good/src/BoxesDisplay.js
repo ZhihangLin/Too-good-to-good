747fb0291e0b560719640667dae85e4f9e036cce
ZhihangLin <93102378+ZhihangLin@users.noreply.github.com>
2024-03-24 00:26:40 -0400

    Update BoxesDisplay.js

--- Too_Good_To_Good/src/BoxesDisplay.js
+++ Too_Good_To_Good/src/BoxesDisplay.js
@@ -49,15 +49,15 @@ function BoxesDisplay() {
                 <div key={box.id} className="box1">
                     <img src={box.imageUrl || 'https://firebasestorage.googleapis.com/v0/b/tgtg-af1a6.appspot.com/o/images%2Ftransparency_demonstration_1.png?alt=media&token=dde7538e-df6d-47f8-ae0b-4c0df81c4b8d'} alt={box.type} />
                     <div className="boxDetails">
-                        <h3>{box.type}</h3>
-                        <p>Product Name: 
-                            <input type="text" value={box.productName} onChange={(e) => handleValueChange(box.id, 'productName', e.target.value)} />
+                        <h3>{box.productName}</h3>
+                        <p>Type: 
+                            <input type="text" value={box.type} onChange={(e) => handleValueChange(box.id, 'type', e.target.value)} />
                         </p>
                         <p>Origin Price: 
                             <input type="number" value={box.originPrice} onChange={(e) => handleValueChange(box.id, 'originPrice', e.target.value)} />
                         </p>
                         <p>Location: 
-                            <input type="text" value={box.Location} onChange={(e) => handleValueChange(box.id, 'location', e.target.value)} />
+                            <input type="text" value={box.location} onChange={(e) => handleValueChange(box.id, 'location', e.target.value)} />
                         </p>
                         <p>Notes: 
                             <input type="text" value={box.notes} onChange={(e) => handleValueChange(box.id, 'notes', e.target.value)} />
