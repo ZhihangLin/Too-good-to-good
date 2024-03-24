218e70df8c2814908034e661626b257a7178c612
Gangkui777 <124069863+Gangkui777@users.noreply.github.com>
2024-03-23 23:47:44 -0400

    Add files via upload

--- Too_Good_To_Good/src/BoxesDisplay.js
+++ Too_Good_To_Good/src/BoxesDisplay.js
@@ -46,7 +46,7 @@ function BoxesDisplay() {
     return (
         <div className="boxesDisplay">
             {boxes.map((box) => (
-                <div key={box.id} className="box">
+                <div key={box.id} className="box1">
                     <img src={box.imageUrl || 'https://firebasestorage.googleapis.com/v0/b/tgtg-af1a6.appspot.com/o/images%2Ftransparency_demonstration_1.png?alt=media&token=dde7538e-df6d-47f8-ae0b-4c0df81c4b8d'} alt={box.type} />
                     <div className="boxDetails">
                         <h3>{box.type}</h3>
@@ -56,7 +56,9 @@ function BoxesDisplay() {
                         <p>Origin Price: 
                             <input type="number" value={box.originPrice} onChange={(e) => handleValueChange(box.id, 'originPrice', e.target.value)} />
                         </p>
-                        <p>Location: {box.location}</p>
+                        <p>Location: 
+                            <input type="text" value={box.Location} onChange={(e) => handleValueChange(box.id, 'location', e.target.value)} />
+                        </p>
                         <p>Notes: 
                             <input type="text" value={box.notes} onChange={(e) => handleValueChange(box.id, 'notes', e.target.value)} />
                         </p>
@@ -72,4 +74,3 @@ function BoxesDisplay() {
 }
 
 export default BoxesDisplay;
-
