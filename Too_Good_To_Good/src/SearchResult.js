ec09921cc103ed4076d9abea1b519f31dc7900b2
ZhihangLin <93102378+ZhihangLin@users.noreply.github.com>
2024-05-13 00:13:43 -0400

    Add files via upload

--- Too_Good_To_Good/src/SearchResult.js
+++ Too_Good_To_Good/src/SearchResult.js
@@ -45,7 +45,7 @@ function SearchResult() {
                 boxesWithImages.map((box) => (
                     <Link to={`/result/${box.id}`} key={box.id} style={{ textDecoration: 'none' }}>
                         <div className="box1">
-                            <img src={box.imageUrl || 'placeholder-image-url'} alt={box.type} />
+                            <img src={'https://images-ext-1.discordapp.net/external/GuEfrENZrrYJocJMEA0jHxVd0HLEVfTeMokIXsSKkrE/%3Fid%3DOIP.iqldYf72fpKKy0NYd9wVkAHaJH%26pid%3DApi%26P%3D0%26h%3D180/https/tse4.explicit.bing.net/th?format=webp&width=219&height=270'} />
                             <div className="boxDetails">
                                 <h3>{box.productName}</h3>
                                 <p>Type: {box.type}</p>
